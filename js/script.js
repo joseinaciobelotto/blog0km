db9919d69514a27140f96ef8e16de54710a94bd4
zeapling <121182225+zeapling@users.noreply.github.com>
2024-05-24 10:19:30 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ const arrayTesteImagens = ["./images/Nova-Strada-Ultra-2024.jpg","./images/lambo
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg"]
 
+
 let containerPostsMain = document.querySelector(".mainPosts");
 let containerPostsSolo = document.querySelector(".mainPostsSolo");
 let divDestaque = document.querySelector(".divDestaque");
-let headerDiv = document.querySelector(".headerDiv");
-let botaoSand = document.querySelector(".botaoSand");
+
+
 
 let indexmodified=0;
 let auxDiv=1;
@@ -19,7 +20,6 @@ let numeorDePost= arrayTeste.length;
 let positionArray
 let rowPostsMain;
 
-let tamanhoTela = screen.width/100;
 
 divDestaque.addEventListener("click", (e)=>{
             
@@ -30,21 +30,6 @@ divDestaque.addEventListener("click", (e)=>{
     localStorage.setItem('indexPost',  positionArray);
 });
 
-if(screen.width>991)
-    {
-        headerDiv.style.display="flex";
-    }
-
-botaoSand.addEventListener("click", (e)=>{
-    if(headerDiv.style.display=="flex")
-        {
-            headerDiv.style.display="none";
-        }
-        else{
-            headerDiv.style.display="flex";
-        }
-    
-});
 
 
 function mostrarPosts() 
@@ -74,26 +59,34 @@ function mostrarPosts()
             postsDiv.className = "col-3 meio postsP";
         }
         let postsP = document.createElement("p");
-       
+       // let postsPinfo = document.createElement("p");
         let postImgP = document.createElement("div");
-      
-        postImgP.style.backgroundImage="url("+arrayTesteImagens[i]+")";
-        postImgP.style.height="15vw";
-        postImgP.style.overflow="hidden"
-        postImgP.style.borderRadius="0px 0px 0px 0px";
-        postImgP.style.backgroundSize="140%";
-        postImgP.style.width="auto";
-        postImgP.style.backgroundPosition="center";
-        postsDiv.appendChild(postImgP);
-        postsDiv.appendChild(postsP)   
-        
-        postsP.innerHTML= element;
+      /*  postsPinfo.innerHTML="Saber Mais"
+        postsPinfo.style.fontSize= "120%";
+        postsPinfo.style.textAlign= "center";
+        postsPinfo.style.paddingTop= "1vw";*/
+    postImgP.style.backgroundImage="url("+arrayTesteImagens[i]+")";
+    postImgP.style.height="300px";
+    postImgP.style.overflow="hidden"
+    postImgP.style.borderRadius="0px 0px 0px 0px";
+  //  postImgP.style.boxShadow="0px -30px 30px 0px rgba(255, 255, 255, 0.34)";
+   // postImgP.style.mixBlendMode="diference";
+    postImgP.style.backgroundSize="140%";
+    postImgP.style.width="auto";
+    postImgP.style.backgroundPosition="center";
+ postsDiv.appendChild(postImgP);
+    postsDiv.appendChild(postsP)
 
-        postsP.style.fontSize= "x-larger";
+       
+       //postsDiv.appendChild(postsPinfo)
+
+        postsP.innerHTML= element;
+        postsP.style.fontSize= "140%";
         postsP.style.paddingBlock= "1vw";
         postsDiv.style.height = "";
-      
-        postsDiv.style.width = "400px";
+       // postsDiv.style.backgroundImage="url("+arrayTesteImagens[i]+")";
+       // postsDiv.style.backgroundSize="1000%"
+        postsDiv.style.width = "48%";
         rowPostsMain.appendChild(postsDiv);
 
         containerPostsMain.appendChild(rowPostsMain);
@@ -114,6 +107,7 @@ function mostrarPosts()
     });
 
 };
+
 
 mostrarPosts();
 
--- js/script2.js
+++ js/script2.js
@@ -5,27 +5,11 @@ const arrayTesteImagens = ["./images/xiaomi-su7.jpg","./images/Nova-Strada-Ultra
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg"]
 
+
 let containerPostsMainHere = document.querySelector(".mainPosts2");
 let containerPostsSoloHere = document.querySelector(".mainPostsSolo2");
 
-let headerDiv = document.querySelector(".headerDiv");
-let botaoSand = document.querySelector(".botaoSand");
 
-if(screen.width>991)
-    {
-        headerDiv.style.display="flex";
-    }
-    
-botaoSand.addEventListener("click", (e)=>{
-    if(headerDiv.style.display=="flex")
-        {
-            headerDiv.style.display="none";
-        }
-        else{
-            headerDiv.style.display="flex";
-        }
-            
-});
 
 let auxArray = localStorage.getItem('indexPost');
 
@@ -41,28 +25,31 @@ function mostrarUmCerto(){
     rowPostsMain2.style.justifyContent = "space-around";
   
     let postsDiv2 = document.createElement("div");
-    postsDiv2.className = "col-9 postsP2";
+    postsDiv2.className = "col-9 postsP3";
     let postsP2 = document.createElement("p");
     let postsPinfo2 = document.createElement("p");
     let postImgP2 = document.createElement("img");
-    
+    postsP2.className="p2titulo";
+
     postsPinfo2.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue dui, finibus a condimentum nec, aliquam vitae metus. Integer iaculis nisl sed mollis malesuada. Aliquam nisl elit, suscipit ut congue vel, bibendum ac mi. Suspendisse vitae ultrices velit, eu auctor neque. Donec porttitor eu leo ut bibendum. Phasellus laoreet ligula sit amet sem viverra efficitur. Vivamus aliquet pretium mauris, eget elementum metus semper ac. Morbi consequat, augue vitae consectetur pharetra, ex arcu elementum neque, ut lacinia dolor lorem at ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus."
     postsPinfo2.style.fontSize= "120%";
     postsPinfo2.style.textAlign = "justify";
     postsPinfo2.style.paddingInline = "";
-    postsPinfo2.style.paddingBottom = "6vw";
-    postImgP2.src=arrayTesteImagens[auxArray];
-    postImgP2.style.width="100%";
+    postsPinfo2.style.paddingBottom = "1vw";
+postImgP2.src=arrayTesteImagens[auxArray];
+postImgP2.style.width="100%";
 
-    postImgP2.style.paddingBottom="2vw";
-    postImgP2.style.paddingInline="";
-    postImgP2.style.borderRadius=" ";
 
-    postsDiv2.appendChild(postImgP2);
-    postsDiv2.appendChild(postsP2);
-    postsDiv2.appendChild(postsPinfo2);
+postImgP2.style.paddingBottom="2vw";
+postImgP2.style.paddingInline="";
+postImgP2.style.borderRadius=" ";
+
+   postsDiv2.appendChild(postImgP2);
+   postsDiv2.appendChild(postsP2);
+   postsDiv2.appendChild(postsPinfo2);
 
     postsP2.innerHTML= arrayTesteHere[auxArray];
+  
     postsP2.style.fontSize= "160%";
     postsDiv2.style.height = "auto";
     postsDiv2.style.width = "100%";
@@ -74,5 +61,6 @@ function mostrarUmCerto(){
 }
 
 mostrarUmCerto();
+
 
 //
