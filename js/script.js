17b3ef8a353566e5f15ae29c9d9ec646bada63fe
zeapling <121182225+zeapling@users.noreply.github.com>
2024-05-24 12:13:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-const arrayTeste = ["TÍTULO AAA","TÍTULO BBB","TÍTULO CCC" ,"TÍTULO DDD","TÍTULO EEE","TÍTULO FFF","TÍTULO GGG","TÍTULO HHH","TÍTULO III","TÍTULO JJJ","TÍTULO KKK","TÍTULO LLL","TÍTULO MMM"];
+const arrayTeste = ["Aston Martin: Luxo e Performance em Cada Detalhe", "Bentley: O Pináculo do Conforto e Elegância", "BMW: Inovação e Dinamismo na Estrada", "Bugatti: Velocidade Extrema e Exclusividade", "Cadillac: Estilo e Sofisticação Americana", "Chevrolet: Confiabilidade e Versatilidade", "Dodge: Potência e Desempenho para Aventureiros", "Ferrari: A Emoção de Dirigir um Superesportivo", "Ford: Tradição e Modernidade em Movimento", "Honda: Tecnologia e Eficiência para o Dia a Dia", "Hyundai: Conforto e Segurança para sua Família", "Jaguar: Elegância e Potência Britânica", "Jeep: Aventuras Sem Limites com Robustez", "Lamborghini: O Poder da Exclusividade Italiana", "Land Rover: Sofisticação e Versatilidade Off-Road", "Lexus: Luxo e Inovação em Cada Modelo"];
 const arrayTesteImagens = ["./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg"]
 
-
 let containerPostsMain = document.querySelector(".mainPosts");
 let containerPostsSolo = document.querySelector(".mainPostsSolo");
 let divDestaque = document.querySelector(".divDestaque");
-
-
+let headerDiv = document.querySelector(".headerDiv");
+let botaoSand = document.querySelector(".botaoSand");
 
 let indexmodified=0;
 let auxDiv=1;
@@ -19,6 +18,9 @@ let numeorDePost= arrayTeste.length;
 
 let positionArray
 let rowPostsMain;
+
+let tamanhoTela = screen.width/100;
+
 
 
 divDestaque.addEventListener("click", (e)=>{
@@ -30,6 +32,27 @@ divDestaque.addEventListener("click", (e)=>{
     localStorage.setItem('indexPost',  positionArray);
 });
 
+if(screen.width>991)
+    {
+        headerDiv.style.display="flex";
+      
+    }
+    else
+    {
+        headerDiv.style.display="none";
+    }
+   
+
+botaoSand.addEventListener("click", (e)=>{
+    if(headerDiv.style.display=="flex")
+        {
+            headerDiv.style.display="none";
+        }
+        else{
+            headerDiv.style.display="flex";
+        }
+    
+});
 
 
 function mostrarPosts() 
@@ -49,44 +72,45 @@ function mostrarPosts()
      
        
         let postsDiv = document.createElement("div");
+        
 
         postsDiv.style.cursor = "pointer";
         
         if(auxDiv ==2)
         {
-            postsDiv.className = "col-3 meio postsP";
+            postsDiv.className = "col-5 meio postsP postDiv";
         }else{
-            postsDiv.className = "col-3 meio postsP";
+            postsDiv.className = "col-5 meio postsP postDiv";
         }
         let postsP = document.createElement("p");
-       // let postsPinfo = document.createElement("p");
-        let postImgP = document.createElement("div");
-      /*  postsPinfo.innerHTML="Saber Mais"
-        postsPinfo.style.fontSize= "120%";
-        postsPinfo.style.textAlign= "center";
-        postsPinfo.style.paddingTop= "1vw";*/
-    postImgP.style.backgroundImage="url("+arrayTesteImagens[i]+")";
-    postImgP.style.height="300px";
-    postImgP.style.overflow="hidden"
-    postImgP.style.borderRadius="0px 0px 0px 0px";
-  //  postImgP.style.boxShadow="0px -30px 30px 0px rgba(255, 255, 255, 0.34)";
-   // postImgP.style.mixBlendMode="diference";
-    postImgP.style.backgroundSize="140%";
-    postImgP.style.width="auto";
-    postImgP.style.backgroundPosition="center";
- postsDiv.appendChild(postImgP);
-    postsDiv.appendChild(postsP)
-
        
-       //postsDiv.appendChild(postsPinfo)
-
+        let postImgP = document.createElement("img");
+      
+        postImgP.src=arrayTesteImagens[i];
+        postImgP.style.height="auto";
+        postImgP.style.overflow="hidden"
+        postImgP.style.borderRadius="0px 0px 0px 0px";
+    
+        postImgP.style.width="100%";
+        postImgP.style.backgroundPosition="center";
+        postsDiv.appendChild(postImgP);
+        postsDiv.appendChild(postsP)   
+        
         postsP.innerHTML= element;
-        postsP.style.fontSize= "140%";
-        postsP.style.paddingBlock= "1vw";
+
+        postsP.style.fontFamily=" Bebas Neue, sans-serif";
+        postsP.style.letterSpacing="1px";
+        postsP.style.letterSpacing="1px";
+        postsP.style.fontWeight="100"
+        postsP.style.fontSize= "x-larger";
+        postsP.style.paddingBlock= "0.1vw";
+        postsP.style.paddingInline= "0.3vw";
         postsDiv.style.height = "";
-       // postsDiv.style.backgroundImage="url("+arrayTesteImagens[i]+")";
-       // postsDiv.style.backgroundSize="1000%"
-        postsDiv.style.width = "48%";
+      
+
+        divDestaque.style.padding = "0px";
+
+        postsP.style.backgroundColor="  black"
         rowPostsMain.appendChild(postsDiv);
 
         containerPostsMain.appendChild(rowPostsMain);
@@ -107,7 +131,6 @@ function mostrarPosts()
     });
 
 };
-
 
 mostrarPosts();
 
--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,33 @@
-const arrayTesteHere = ["XIAOMI SU7: O CARRO ELÉTRICO QUE SURPREENDEU O MERCADO!","TÍTULO AAA","TÍTULO BBB","TÍTULO CCC" ,"TÍTULO DDD","TÍTULO EEE","TÍTULO FFF","TÍTULO GGG","TÍTULO HHH","TÍTULO III","TÍTULO JJJ","TÍTULO KKK","TÍTULO LLL","TÍTULO MMM"];
+const arrayTesteHere = ["XIAOMI SU7: O CARRO ELÉTRICO QUE SURPREENDEU O MERCADO!","Aston Martin: Luxo e Performance em Cada Detalhe", "Bentley: O Pináculo do Conforto e Elegância", "BMW: Inovação e Dinamismo na Estrada", "Bugatti: Velocidade Extrema e Exclusividade", "Cadillac: Estilo e Sofisticação Americana", "Chevrolet: Confiabilidade e Versatilidade em um Só Lugar", "Dodge: Potência e Desempenho para Aventureiros", "Ferrari: A Emoção de Dirigir um Superesportivo", "Ford: Tradição e Modernidade em Movimento", "Honda: Tecnologia e Eficiência para o Dia a Dia", "Hyundai: Conforto e Segurança para sua Família", "Jaguar: Elegância e Potência Britânica", "Jeep: Aventuras Sem Limites com Robustez", "Lamborghini: O Poder da Exclusividade Italiana", "Land Rover: Sofisticação e Versatilidade Off-Road", "Lexus: Luxo e Inovação em Cada Modelo"];
 const arrayTesteImagens = ["./images/xiaomi-su7.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg","./images/Nova-Strada-Ultra-2024.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg","./images/lamborghini-aventador-s-roadster-2018-11.jpg",
 "./images/Nova-Strada-Ultra-2024.jpg"]
 
-
 let containerPostsMainHere = document.querySelector(".mainPosts2");
 let containerPostsSoloHere = document.querySelector(".mainPostsSolo2");
 
+let headerDiv = document.querySelector(".headerDiv");
+let botaoSand = document.querySelector(".botaoSand");
 
+
+
+if(screen.width>991)
+    {
+        headerDiv.style.display="flex";
+    }
+    
+    botaoSand.addEventListener("click", (e)=>{
+        if(headerDiv.style.display=="flex")
+            {
+                headerDiv.style.display="none";
+            }
+            else{
+                headerDiv.style.display="flex";
+            }
+        
+    });
 
 let auxArray = localStorage.getItem('indexPost');
 
@@ -19,38 +37,39 @@ function mostrarUmCerto(){
     containerPostsMainHere.style.display = "none";
    
     containerPostsSoloHere.style.display = "flex";
+    containerPostsSoloHere.style.backgroundColor="black";
+    
+    containerPostsSoloHere.style.paddingTop="45px";
  
     rowPostsMain2 = document.createElement("div") ;
     rowPostsMain2.className = "row posts2";
     rowPostsMain2.style.justifyContent = "space-around";
   
     let postsDiv2 = document.createElement("div");
-    postsDiv2.className = "col-9 postsP3";
+    postsDiv2.className = "col-9 postsP2";
     let postsP2 = document.createElement("p");
     let postsPinfo2 = document.createElement("p");
     let postImgP2 = document.createElement("img");
-    postsP2.className="p2titulo";
-
+    
     postsPinfo2.innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue dui, finibus a condimentum nec, aliquam vitae metus. Integer iaculis nisl sed mollis malesuada. Aliquam nisl elit, suscipit ut congue vel, bibendum ac mi. Suspendisse vitae ultrices velit, eu auctor neque. Donec porttitor eu leo ut bibendum. Phasellus laoreet ligula sit amet sem viverra efficitur. Vivamus aliquet pretium mauris, eget elementum metus semper ac. Morbi consequat, augue vitae consectetur pharetra, ex arcu elementum neque, ut lacinia dolor lorem at ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-    postsPinfo2.style.fontSize= "120%";
+    postsPinfo2.style.fontSize= "140%";
     postsPinfo2.style.textAlign = "justify";
     postsPinfo2.style.paddingInline = "";
-    postsPinfo2.style.paddingBottom = "1vw";
-postImgP2.src=arrayTesteImagens[auxArray];
-postImgP2.style.width="100%";
+    postsPinfo2.style.paddingBottom = "6vw";
+    postImgP2.src=arrayTesteImagens[auxArray];
+    postImgP2.style.width="100%";
 
+    postImgP2.style.paddingBottom="2vw";
+    postImgP2.style.paddingInline="";
+    postImgP2.style.borderRadius=" ";
 
-postImgP2.style.paddingBottom="2vw";
-postImgP2.style.paddingInline="";
-postImgP2.style.borderRadius=" ";
-
-   postsDiv2.appendChild(postImgP2);
-   postsDiv2.appendChild(postsP2);
-   postsDiv2.appendChild(postsPinfo2);
+    postsDiv2.appendChild(postImgP2);
+    postsDiv2.appendChild(postsP2);
+    postsDiv2.appendChild(postsPinfo2);
 
     postsP2.innerHTML= arrayTesteHere[auxArray];
-  
-    postsP2.style.fontSize= "160%";
+    postsPinfo2.style.color="grey";
+    postsP2.style.fontSize= "180%";
     postsDiv2.style.height = "auto";
     postsDiv2.style.width = "100%";
 
@@ -61,6 +80,5 @@ postImgP2.style.borderRadius=" ";
 }
 
 mostrarUmCerto();
-
 
 //
